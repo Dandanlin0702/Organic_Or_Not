3392c0ea8bdaacf184e7d57dfead936772c523f5
Heidi Wu <heidi.codes@gmail.com>
2017-10-14 16:38:37 -0400

    added model files

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-var express    = require("express"),
-    app        = express(),
-    bodyParser = require("body-parser"),  //body-parser
-    mongoose   = require("mongoose"),
-    //models = require('./models'),
-    seedDB     = require("./seeds");
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"), //body-parser
+  mongoose = require("mongoose"),
+  //models = require('./models'),
+  seedDB = require("./seeds");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb://localhost/organic-or-not_development");
 
-    mongoose.connect("mongodb://localhost/organic-or-not_development");
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({layoutsDir: './views/layouts', defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/views/`);
 
-    const exphbs = require('express-handlebars');
-    app.engine('handlebars', exphbs({
-      layoutsDir: './views/layouts',
-      defaultLayout: 'main',
-    }));
-    app.set('view engine', 'handlebars');
-    app.set('views', `${__dirname}/views/`);
+// Load up all of the controllers
+//const controllers = require('./controllers');
+//app.use(controllers);
 
-    // Load up all of the controllers
-    //const controllers = require('./controllers');
-    //app.use(controllers);
-
-    app.listen((process.env.PORT || 5000), function(){
-      console.log("The App Server Has Started!!!")
-    });
+app.listen((process.env.PORT || 5000), function() {
+  console.log("The App Server Has Started!!!")
+});
