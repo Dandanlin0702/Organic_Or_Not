cc847040f234a476b2c15a5a2171d437c5298284
Risa Toyoshima <Risa.Toyoshima31@myhunter.cuny.edu>
2017-10-14 16:24:31 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),  //body-parser
+    mongoose   = require("mongoose"),
+    //models = require('./models'),
+    seedDB     = require("./seeds");
+
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    mongoose.connect("mongodb://localhost/organic-or-not_development");
+
+    const exphbs = require('express-handlebars');
+    app.engine('handlebars', exphbs({
+      layoutsDir: './views/layouts',
+      defaultLayout: 'main',
+    }));
+    app.set('view engine', 'handlebars');
+    app.set('views', `${__dirname}/views/`);
+
+    // Load up all of the controllers
+    //const controllers = require('./controllers');
+    //app.use(controllers);
+
+    app.listen((process.env.PORT || 5000), function(){
+      console.log("The App Server Has Started!!!")
+    });
