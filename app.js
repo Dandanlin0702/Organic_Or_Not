cc51b5393b5d651a145b5f904680f88af9659855
Risa Toyoshima <Risa.Toyoshima31@myhunter.cuny.edu>
2017-10-15 11:23:42 -0400

    fix

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.set('views', `${__dirname}/views/`);
         //console.log(JSON.stringify(req.body));
         const msgFrom = req.body.From;
         const msgBody = req.body.Body;
-        const numOfMedia = 1;
+        const numOfMedia = req.body.NumMedia;
 
         var message = "";
 
@@ -110,7 +110,7 @@ app.set('views', `${__dirname}/views/`);
                                 <Response>
                                     <Message>
                                         It's Organic! 🤗 Compost it up! Give me your zipcode if you want to know your closest scrap drop off location.
-                                    </Message> 
+                                    </Message>
                                 </Response>
                             `);
                         }
