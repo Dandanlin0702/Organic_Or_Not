2bd41d438c3bf598346a7c8453ebca346eebf919
Heidi Wu <heidi.codes@gmail.com>
2017-10-14 22:24:39 -0400

    zipcode form test

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,6 @@
 const express = require('express');
-//const models = require('../models');
-
+const mongoose = require("mongoose");
+const Locations = require("../models/locations.js");
 
 const router = express.Router();
 
@@ -10,6 +10,18 @@ router.get('/', (req, res) => {
   //   msg: "Successful GET to '/' route"
   // });
   res.render('index');
+});
+
+router.get('/zipcode', (req, res) => {
+  // res.json({
+  //   msg: "Successful GET to '/' route"
+  // });
+  res.render('zipcode');
+});
+
+router.post('/zipcode', (req, res) => {
+  const zip = req.body.zipcode;
+  console.log(zip);
 });
 
 router.post('/', (req, res) => {
